c67b01424b50adf66aee9d55041fc697b2505067
IvanOmelchenkoIP <io25112002@gmail.com>
2022-10-31 18:33:20 +0200

    Implement Car constructor
    Implement Car constructor so that now a car can be created with an
    amount of sits

--- java/src/lab4/vehicles/Car.java
+++ java/src/lab4/vehicles/Car.java
@@ -1,8 +1,27 @@
 package lab4.vehicles;
 
+import java.util.ArrayList;
+
 import lab4.persons.Person;
 
 public abstract class Car<T extends Person> extends Vehicle<T> {
+	
+	private final static int MIN_CAR_SEATS = 1;
+	private final static int MAX_CAR_SEATS = 8; 
+	
+	private int maxSeats;
+	private ArrayList<T> passangers;
+	
+	Car(int seats) {
+		maxSeats = createSeats(seats);
+		passangers = new ArrayList<T>();
+	}
+	
+	private int createSeats(int requested) {
+		if (requested < Car.MIN_CAR_SEATS) return Car.MIN_CAR_SEATS; 
+		if (requested > Car.MAX_CAR_SEATS) return Car.MAX_CAR_SEATS;
+		return requested;
+	}
 	
 	public abstract int getMaxSeats();
 	
