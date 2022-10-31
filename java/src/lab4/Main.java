a183a8fab11ebdaeb505a9dce3bb7d784b1bc585
IvanOmelchenkoIP <io25112002@gmail.com>
2022-10-31 20:29:11 +0200

    Add some code to main()
    Add some code to main() to test creation of different classes with
    generics

--- java/src/lab4/Main.java
+++ java/src/lab4/Main.java
@@ -1,5 +1,7 @@
 package lab4;
 import lab4.persons.*;
+import lab4.vehicles.*;
+import lab4.road.*;
 
 public class Main {
 
@@ -9,7 +11,25 @@ public class Main {
 		Policeman p3 = new Policeman("Policeman1_name", "Policeman1_surname");
 		System.out.printf("%s\n%s\n%s\n", p1.getPersonData(), p2.getPersonData(), p3.getPersonData());
 		
+		System.out.println(p3 instanceof Person);
 		
+		Bus<Person> bus1 = new Bus<Person>(20);
+		bus1.addPassanger(p1);
+		bus1.addPassanger(p2);
+		bus1.addPassanger(p3);
+		System.out.println(bus1.getTakenSeats());
+		
+		Firetruck<Firefighter> firetruck1 = new Firetruck<Firefighter>(20);
+		//firetruck1.addPassanger(p2);
+		
+		Policecar<Policeman> policecar1 = new Policecar<Policeman>(20);
+		policecar1.addPassanger(p3);
+		System.out.println(policecar1.getTakenSeats());
+		
+		Road road = new Road();
+		road.addVehicleToRoad(bus1);
+		road.addVehicleToRoad(policecar1);
+		System.out.println(road.getCountOfHumans());
 	}
 
 }
