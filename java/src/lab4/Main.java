2e96e96c6fc4ed17a1478f4725a4254f5f319a12
IvanOmelchenkoIP <io25112002@gmail.com>
2022-11-01 23:12:17 +0200

    Change main()
    Change main() so that it better resembles some hypothetical usecase of
    classes

--- java/src/lab4/Main.java
+++ java/src/lab4/Main.java
@@ -7,32 +7,44 @@ import lab4.road.*;
 public class Main {
 
 	public static void main(String[] args) {
-		Person p1 = new Person("Civillian1_name", "Civillian1_surname");
-		Person p2 = new Firefighter("Firefighter_name", "Firefighter1_surname");
-		Policeman p3 = new Policeman("Policeman1_name", "Policeman1_surname");
-		System.out.printf("%s\n%s\n%s\n", p1.getPersonData(), p2.getPersonData(), p3.getPersonData());
-		
-		System.out.println(p3 instanceof Person);
+		Road road = new Road();
 		
 		Bus<Person> bus1 = new Bus<Person>(20);
-		bus1.addPassanger(p1);
-		bus1.addPassanger(p2);
-		bus1.addPassanger(p3);
-		System.out.println(bus1.getTakenSeats());
+		Firetruck<Firefighter> firetruck1 = new Firetruck<Firefighter>(4);
 		
-		Firetruck<Firefighter> firetruck1 = new Firetruck<Firefighter>(20);
-		firetruck1.addPassanger((Firefighter)p2);
-		System.out.println(firetruck1.getTakenSeats());
+		Person person1 = new Person("Name1", "Surname1");
+		Person person2 = new Person("Name2", "Surname2");
+		Person person3 = new Person("Name3", "Surname3");
+		bus1.addPassanger(person1);
+		bus1.addPassanger(person2);
+		bus1.addPassanger(person3);
 		
-		Policecar<Policeman> policecar1 = new Policecar<Policeman>(20);
-		policecar1.addPassanger(p3);
-		System.out.println(policecar1.getTakenSeats());
+		Policeman policeman1 = new Policeman("Name4", "Surname5");
+		bus1.addPassanger(policeman1);
 		
-		Road road = new Road();
-		road.addVehicleToRoad(bus1);
+		Firefighter firefighter1 = new Firefighter("Name5", "Surname5");
+		Firefighter firefighter2 = new Firefighter("Name6", "Surname6");
+		firetruck1.addPassanger(firefighter1);
+		firetruck1.addPassanger(firefighter2);
+		
 		road.addVehicleToRoad(firetruck1);
+		road.addVehicleToRoad(bus1);
+		
+		System.out.printf("People on 1st part of the road: %d\n", road.getCountOfHumans());
+		
+		Policecar<Policeman> policecar1 = new Policecar<Policeman>(4);
+		
+		Person policeman2 = new Policeman("Name7", "Surname7");
+		policecar1.addPassanger((Policeman)policeman2);
+		bus1.removePassanger(policeman1.getPersonData());
+		policecar1.addPassanger(policeman1);
+		
+		bus1.removePassanger(person1.getPersonData());
+		bus1.removePassanger(person2.getPersonData());
+		
 		road.addVehicleToRoad(policecar1);
-		System.out.println(road.getCountOfHumans());
+		
+		System.out.printf("People on 2nd part of the road: %d\n", road.getCountOfHumans());
 	}
 
 }
